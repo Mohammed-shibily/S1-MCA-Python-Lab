d085363e2a8848b76e69519ee6e0618daeff35cd
Mohammed-shibily <shibilychalambattbackup@gmail.com>
2024-10-30 12:23:57 +0530

    Add files via upload

---
+++ week5/max vlue and min value.py
@@ -0,0 +1,22 @@
+# Sample list
+numbers = [15, 22, 19, 61, 47, 54]
+
+# Find max value
+max_value = max(numbers)
+print("Max value:", max_value)
+
+# Find min value
+min_value = min(numbers)
+print("Min value:", min_value)
+
+# Count sum of the list
+total_sum = sum(numbers)
+print("Sum:", total_sum)
+
+# Reverse the list
+reversed_list = list(reversed(numbers))
+print("Reversed list:", reversed_list)
+
+# Sort the list
+sorted_list = sorted(numbers)
+print("Sorted list:", sorted_list)
