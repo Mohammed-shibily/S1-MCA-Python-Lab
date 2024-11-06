37802c112d44e2ffbefc2bbbe17f3525b4280b7d
Mohammed-shibily <shibilychalambattbackup@gmail.com>
2024-11-06 15:32:55 +0530

    Add files via upload

---
+++ week6/lambda radius.py
@@ -0,0 +1,3 @@
+x=lambda a:3.14*(a*a)
+radius=int(input("Enter The Radius= "))
+print("The Area of circle is= ",x(radius)) 
---
+++ week6/lambda.py
@@ -0,0 +1,3 @@
+x=lambda a:a*a*a
+w=int(input("enter a number: "))
+print("The Cube of The ",w," is =",x(w))
